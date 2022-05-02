9f8b8d02921cd2b603a8963a909807e22dccb07d
billwk254 <billkihungu51@gmail.com>
2022-05-02 18:31:43 +0300

    A class that tracks statistics for the game with a method to reset all statistics

---
+++ game_stats.py
@@ -0,0 +1,17 @@
+class GameStats():
+    """Track statistics for alien invasion"""
+    def __init__(self,ai_settings):
+        """Initialize statistics"""
+        self.ai_settings = ai_settings
+        self.reset_stats()
+        #The high score should never be reset
+        self.high_score = 0
+
+    def reset_stats(self):
+        """Initialize statistics that can change during the game"""
+        self.ships_left = self.ai_settings.ship_limit
+        #Start the game in an inactive state
+        self.game_active = False
+        self.score = 0
+        self.level = 1
+
